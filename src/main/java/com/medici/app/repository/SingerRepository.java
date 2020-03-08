5dfd3cbc8dd66a1e931a7a1b3dad819ea6a8f94d
TOSS <tiago.medice@gft.com>
2020-03-08 18:41:11 +0100

    release

--- src/main/java/com/medici/app/repository/SingerRepository.java
+++ src/main/java/com/medici/app/repository/SingerRepository.java
@@ -1,7 +1,5 @@
 package com.medici.app.repository;
 
-import java.util.List;
-
 import org.springframework.cloud.gcp.data.datastore.repository.DatastoreRepository;
 import org.springframework.cloud.gcp.data.datastore.repository.query.Query;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +7,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.medici.app.entity.Band;
 import com.medici.app.entity.Singer;
 
 @RepositoryRestResource(collectionResourceRel = "singers", path = "singer")
@@ -18,8 +15,4 @@ public interface SingerRepository extends DatastoreRepository<Singer, String> {
 	@Query("select * from  |com.medici.app.entity.Singer| where last_name = @name")
 	Slice<Singer> findSingersByLastName(@Param("name") String name, Pageable pageable);
 
-	@Query("select * from  |com.medici.app.entity.Singer| where firstBand = @band")
-	List<Singer> findSingersByFirstBand(@Param("band") Band band);
-
-	List<Singer> findByFirstBand(Band band);
 }
