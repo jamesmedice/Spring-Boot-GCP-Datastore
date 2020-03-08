79c435eae1ae1934802958b0d47c24dc06fc2f3b
TOSS <tiago.medice@gft.com>
2020-03-08 18:20:43 +0100

    release

--- src/main/java/com/medici/app/config/SwaggerConfig.java
+++ src/main/java/com/medici/app/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.medici.app.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
@@ -9,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
  */
 @Configuration
 @EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class SwaggerConfig {
 
 }
